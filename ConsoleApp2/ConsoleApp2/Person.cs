82e5e26c5d210df73dc787fb639f110cdbd89083
Elev <Elev@TEB126006>
2019-12-06 12:54:03 +0100

    färdig och har lagt upp uppgiften

---
+++ ConsoleApp2/ConsoleApp2/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Person
+    {
+        private string namn;
+        private int ålder;
+        private string adress;
+        //Konstruktor som tar tre parametrar
+
+        public Person(string n, int å, string a)
+        {
+            namn = n;
+            ålder = å;
+            adress = a;
+        }
+        public Person() { }  //konstruktor som inte gör någontong
+        public string Namn //Egenskap för datamedlemmen namn
+        {
+            get { return namn;  }
+            set { namn = value; }
+        }
+
+        public int Ålder
+        {
+            get { return ålder; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Fel! Åldern måste vara positiv");
+                    ålder = 0;
+                }
+                else
+                    ålder = value;
+            }
+        }
+        public string Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static object personllista;
+        private static object m;
+
+        public static void Main()
+        {
+            List<Person> personlista = new List<Person>();
+
+            Person p = new Person("Luke Skywalker", 20, "Moisture farm 1138, Tatooine");
+            personlista.Add(p);
+            personlista.Add(new Person("Lando Calrissian", 28, "Cloud City, Bespin"));
+            personlista.Add(new Person("Marko Popara", 19, "Växjö, Sverige"));
+            personlista.Add(new Person("BMW 328Ci (E46)", 1999, "Germany"));
+            personlista.Add(new Person("Honda Civic (D15Z6)", 1999, "Japan"));
+            foreach (Person element in personlista)
+            {
+
+                Console.WriteLine(element.Namn);
+                Console.WriteLine(element.Ålder);
+                Console.WriteLine(element.Adress);
+                Console.WriteLine("____________________");
+            }
+        }
+
+        private static Person NewMethod()
+        {
+            return new Person("Luke skywalker", 20, "Moisture farm 1138, Tatooine");
+        }
+    }
+}
